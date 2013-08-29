970b8beb388d208e8ac26fe4c89759b9d9e9b92d
Ben-Hadfield <bjamin.nz@gmail.com>
2013-08-29 22:14:48 +0800

    maybe this works...

--- lesson13/labs/loopsExercise/js/exercise.js
+++ lesson13/labs/loopsExercise/js/exercise.js
@@ -10,6 +10,10 @@
  */
    var myArray = [];
 
+for (i = 1; i <= 10; i++) {
+ myArray.push(i);
+}
+
 
 
 /**
@@ -20,7 +24,10 @@
 var sub = 100;
 var start = 1;
 
-
+while (start <= 10) {
+	sub = sub - start;
+	start++;
+}
 
 /**
  * Question 3
@@ -30,7 +37,13 @@ var start = 1;
 var someNums = [3, 1, 88, -1, 33, 96, 35, 12, 72];
 var largest_num = someNums[0];
 
+for (var i = 0; i < someNums.length; i++) {
+	if (largest_num < someNums[i] )
+		largest_num = someNums[i];
 
+}
+
+console.log (largest_num)
 
 /**
  * Question 4
@@ -38,6 +51,16 @@ var largest_num = someNums[0];
  * Store that number in "small"
  */
 var small = someNums[0];
+
+for (var i = 0; i < someNums.length; i++) {
+	if (small > someNums[i] )
+		small = someNums[i];
+
+}
+
+console.log (small)
+
+
 
 /**
  * Question 5
@@ -49,6 +72,14 @@ var firstArray = ['peaches', 'apples', 'pears', 'oranges', 'plums', 'water melon
 var secondArray = ['blueberries', 'water melon', 'tomatoes', 'apples', 'plums', 'oranges', 'cantaloupe'];
 var totalDuplicates = 0;
 
+for (var i = 0; i < firstArray.length; i++) {
+	for (j = 0; j < secondArray.length; j++) {
+		if (firstArray[i] == secondArray[j])
+			totalDuplicates++;
+
+	}
+}
+		console.log(totalDuplicates)
 
 /**
  * Question 6 - ******BONUS******
@@ -59,4 +90,15 @@ var totalDuplicates = 0;
 
 var numbers = [3, 7, 1, 3, 23, 7, 98, 26, 1];
 var totalUniques = 0;
+
+for (var i = 0; i < numbers.length; i++) {
+	if (numbers[i] == numbers[i])
+		totalUniques++;
+
+}
+console.log(totalUniques)
+
+
+
+
 
--- lesson14/labs/exerciseReturnFunctions/js/exercise.js
+++ lesson14/labs/exerciseReturnFunctions/js/exercise.js
@@ -7,7 +7,7 @@
  * Modify the "booleanValue" function so that it returns "true" when it's called.
  */
 function booleanValue() {
-  return false;
+  return true;
 }
 
 
@@ -17,7 +17,7 @@ function booleanValue() {
  * Notice that the function is being executed and the result stored in the variable "q2"
  */
 function helloWorld() {
-
+ return "Hello World!"
 }
 var q2 = helloWorld();
 
@@ -28,7 +28,7 @@ var q2 = helloWorld();
  * Notice that the result of that function is being stored in q3
  */
 function join(left, right) {
-
+ return left + right;
 }
 var q3 = join("1 + 1 = ", "2");
 
@@ -40,11 +40,23 @@ var q3 = join("1 + 1 = ", "2");
  * with the number 42 and 3257 and store the result in variable "q4".
  */
 
+function multiply(x, y) {
+	return x * y
+
+}
+var q4 = multiply(42, 3257)
+
 
 /**
  * Question 5:
  * Write a function named "smallNumber" that compares two number and returns that one. 
  */
+
+function smallNumber(x, y) {
+	return x < y ? x : y;
+
+}
+ var q5 = smallNumber(2, 5)
 
 
 /**
@@ -52,7 +64,11 @@ var q3 = join("1 + 1 = ", "2");
  * Similar to question 5, write a function called "bigNumber" which returns the larger of the
  * two number passed in as arguments
  */
+function bigNumber(x, y) {
+	return x > y ? x : y;
 
+}
+ var q6 = bigNumber(2, 5)
 
 /**
  * Question 7:
@@ -63,6 +79,15 @@ var q3 = join("1 + 1 = ", "2");
  */
 var testArray = [1, 5, 4, 3, 2, 9, 8, 7, 6, 10];
 
+function addArray(arr) {
+	var result = 0;
+	for (i = 0; i < arr.length; i++) {
+		result = result + arr[i];
+
+	}
+return result;
+
+}
 
 /**
  * Question 8:
@@ -71,4 +96,22 @@ var testArray = [1, 5, 4, 3, 2, 9, 8, 7, 6, 10];
  * Hint: You'll need to keep track of the largest number you found so far as 
  *       you step through the array
  */
+
+function theBigOne(anArray) {
+	var max = anArray[0];
+
+	for (i = 0; i < anArray.length; i++) {
+		if (max < anArray[i]) {
+			max = anArray[i];
+		}
+
+	}
+
+return max;
+}
+
+
+
+
+
 

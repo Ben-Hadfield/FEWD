86caead55f4a7f350b97daeeb6c697e47ac22fde
Mart van de Ven <m@type.hk>
2013-09-13 20:57:08 +0400

    Use Array literals, or Array.push(item) to add more items to an array

--- final/Ben/js/script.js
+++ final/Ben/js/script.js
@@ -18,28 +18,28 @@ $('a.category').click(
 
 var current_date = new Date ( );
 
-var month_names = new Array ( );
-month_names[month_names.length] = "January";
-month_names[month_names.length] = "February";
-month_names[month_names.length] = "March";
-month_names[month_names.length] = "April";
-month_names[month_names.length] = "May";
-month_names[month_names.length] = "June";
-month_names[month_names.length] = "July";
-month_names[month_names.length] = "August";
-month_names[month_names.length] = "September";
-month_names[month_names.length] = "October";
-month_names[month_names.length] = "November";
-month_names[month_names.length] = "December";
+var month_names = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"]
 
-var day_names = new Array ( );
-day_names[day_names.length] = "Sunday";
-day_names[day_names.length] = "Monday";
-day_names[day_names.length] = "Tuesday";
-day_names[day_names.length] = "Wednesday";
-day_names[day_names.length] = "Thursday";
-day_names[day_names.length] = "Friday";
-day_names[day_names.length] = "Saturday";
+var day_names = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"]
 
 var date = day_names[current_date.getDay()] + ' ' + month_names[current_date.getMonth()] + ' ' + current_date.getDate();
 $('#date').text(date);

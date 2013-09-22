f759511b1fb0f74f70d7c3225972ffd641f19097
Mart van de Ven <m@type.hk>
2013-09-23 01:03:11 +0200

    Use 'indexOf' for checking array membership

--- final/Ben/js/script.js
+++ final/Ben/js/script.js
@@ -137,8 +137,6 @@ $(".arrowright, .arrowleft").click(function() {
 	}
 
 	prepareAds();
-	refreshAds();
-
 });
 
 // 6. Prepare Ads
@@ -158,7 +156,7 @@ function prepareAds() {
 	$(".contentwrapper").html('');
 
     for (var i = 0; i < todaysDeals.length; i++) {
-      $(".contentwrapper").prepend('<div class="ad ad-' + i + ' ' + todaysDeals[i].adSize + '"></div>');
+      $(".contentwrapper").prepend('<div id ="ad-' + todaysDeals[i].id + '" class="ad ad-' + i + ' ' + todaysDeals[i].adSize + '"></div>');
       $(".ad-" + i).html('<div class="head"></div><div class="promopic"></div><div class="foot"></div><div class="contactinfo"></div><div class="socialbuttons"></div><div class="adMap"></div>');
       $(".ad-" + i + " .head").html('<div class="floatleft"><p>' + todaysDeals[i].category + '</p></div><div class="districtDeal floatright"><p>' + todaysDeals[i].district + '</p></div>');
       $(".ad-" + i + " .promopic").html('<img src=' + todaysDeals[i].mainImg + '>');
@@ -178,7 +176,7 @@ function prepareAds() {
 //
 
 
-$("#locationsubmitfield").click(function() {
+$("#locationsubmitfield").on('click', function() {
 
 	var checkLocation = []
 	$("input[id='adDistrictCheck']:checked").each(function ()
@@ -186,23 +184,15 @@ $("#locationsubmitfield").click(function() {
 	    checkLocation.push($(this).val());
 	});
 
-for (var i = 0; i < deals.length; i++) {
-	for (j = 0; j < checkLocation.length; j++) {
-		if (deals[i] === checkLocation[j])
-			$(".ad-" + i).css("display","none")	;
-
+	for (var i = 0; i < deals.length; i++) {
+		if (checkLocation.indexOf(deals[i].district) == -1) {
+			$("#ad-" + i).hide();
+		} else {
+			$("#ad-" + i).show();
+		}
 	}
-}
-
-	// for (var i = 0; i < deals.length; i++) {
-	// 	if (checkLocation[i] != deals[i].district) {
-	// 	$(".ad-" + i).css("display","none")	
-	// 	};
-	// };
 
 	$('.locationbar').slideToggle();
-		console.log(checkLocation)
-
 });
 
 // 7.1 Clear location
